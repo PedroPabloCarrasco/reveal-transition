4584a9330ef4a2a7b009edcdd390b56ec428b50f
PedroPablo <pedropablocarrascob@gmail.com>
2024-08-20 12:45:55 -0400

    Se agregan todas las animaciones de la libreria en el proyecto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
 import Zoom from 'react-reveal/Zoom';
 import Bounce from 'react-reveal/Bounce';
+import Rotate from 'react-reveal/Rotate';
+import Fade from 'react-reveal/Fade';
+import Slide from 'react-reveal/Slide';
+import Flip from 'react-reveal/Flip';
+import Roll from 'react-reveal/Roll';
+import LightSpeed from 'react-reveal/LightSpeed';
 
 const App = () => {
   return (
     <div>
       <section>
         <h3>Título 1</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula mauris nec neque gravida, ac blandit velit eleifend.
-        </p>
+        <Fade>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vehicula mauris nec neque gravida, ac blandit velit eleifend.
+          </p>
+        </Fade>
       </section>
 
       <section>
         <h3>Título 2</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec lacus a est efficitur feugiat id et erat.
-        </p>
+        <Bounce>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec lacus a est efficitur feugiat id et erat.
+          </p>
+        </Bounce>
       </section>
 
       <Zoom>
@@ -30,9 +40,47 @@ const App = () => {
 
       <section>
         <h3>Título 4</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor elit vitae dictum vehicula.
-        </p>
+        <Slide left>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor elit vitae dictum vehicula.
+          </p>
+        </Slide>
+      </section>
+
+      <section>
+        <h3>Título 5</h3>
+        <Flip>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor elit vitae dictum vehicula.
+          </p>
+        </Flip>
+      </section>
+
+      <section>
+        <h3>Título 6</h3>
+        <Roll>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor elit vitae dictum vehicula.
+          </p>
+        </Roll>
+      </section>
+
+      <Rotate>
+        <section>
+          <h3>Título 7</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor elit vitae dictum vehicula.
+          </p>
+        </section>
+      </Rotate>
+
+      <section>
+        <h3>Título 8</h3>
+        <LightSpeed>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor elit vitae dictum vehicula.
+          </p>
+        </LightSpeed>
       </section>
     </div>
   );
